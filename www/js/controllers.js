ee409f9c04358128046c17b61095aeb9a0ec1563
Chiu Yong <cyc@cin.ufpe.br>
2016-05-16 21:45:39 -0300

    Desenvolvimento da Feature e Conclusão

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,96 +1,69 @@
+var postsConfig = {
+  nome: "Anônimo",
+  foto: "img/ionic.png"
+};
+
 angular.module('starter.controllers', [])
 
-.controller('PublicarCtrl', function($scope) {})
+.controller('PublicarCtrl', function($scope, $ionicPopup, Publicacoes) {
+  // add: function(nome, descricao, face, foto) {
+    $scope.publicar = function(postagem) {
+      if (postagem.descricao) {
+        Publicacoes.add(postsConfig.nome, postagem.descricao, postsConfig.foto, postagem.foto);
+        postagem.descricao = null;
+        postagem.foto = null;
+
+        var alertPopup = $ionicPopup.alert({
+           title: 'Post publicado com sucesso',
+           // template: 'It might taste good'
+         });
+
+         // alertPopup.then(function(res) {
+         //   console.log('Thank you for not eating my delicious ice cream cone');
+         // });
+        }
+    };
+})
 
 .controller('FiscalizarCtrl', function($scope, Publicacoes) {
-  // Com a nova view cache no Ionic, Controllers só são chamados quando eles são 
-  // criados ou quando o aplicativo é iniciado, em vez de a cada mudança de página. Para ouvir 
-  // quando esta página está ativa (por exemplo, para atualizar os dados), ouvir o 
+  // Com a nova view cache no Ionic, Controllers só são chamados quando eles são
+  // criados ou quando o aplicativo é iniciado, em vez de a cada mudança de página. Para ouvir
+  // quando esta página está ativa (por exemplo, para atualizar os dados), ouvir o
   // evento $ionicView.enter:
   //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  $scope.$on('$ionicView.enter', function(e) {
+  });
 
   $scope.publicacoes = Publicacoes.all();
+
+  $scope.verificarApoio = function(id) {
+    return Publicacoes.verificarApoio(id, postsConfig.nome)
+  };
+
+  $scope.apoiarToggle = function(id) {
+    Publicacoes.apoiarToggle(id, postsConfig.nome);
+  };
 })
 
 .controller('PublicacaoDetailCtrl', function($scope, $stateParams, Publicacoes) {
   $scope.publicacao = Publicacoes.get($stateParams.publicacaoId);
+
+  $scope.add = function(comment) {
+    if (comment.message) {
+      Publicacoes.addComment($stateParams.publicacaoId, postsConfig.nome, comment.message);
+      comment.message = null;
+    }
+  };
 })
 
-.controller('AtividadeCtrl', function($scope) {
- 
-  $scope.data = {
-    showDelete: false
+.controller('ConfiguracaoCtrl', function($scope, Publicacoes) {
+  $scope.config = postsConfig;
+
+  $scope.resetComments = function() {
+    var pubs = Publicacoes.all();
+
+    pubs.forEach(function(pub) {
+      pub.comments = [];
+    });
   };
-  
-  $scope.edit = function(item) {
-    alert('Edita Item: ' + item.id);
-  };
-  $scope.share = function(item) {
-    alert('Divulga Item: ' + item.id);
-  };
-  
-  $scope.moveItem = function(item, fromIndex, toIndex) {
-    $scope.items.splice(fromIndex, 1);
-    $scope.items.splice(toIndex, 0, item);
-  };
-  
-  $scope.onItemDelete = function(item) {
-    $scope.items.splice($scope.items.indexOf(item), 1);
-  };
-  
-  $scope.items = [
-    { id: 0 },
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
-    { id: 6 },
-    { id: 7 },
-    { id: 8 },
-    { id: 9 },
-    { id: 10 },
-    { id: 11 },
-    { id: 12 },
-    { id: 13 },
-    { id: 14 },
-    { id: 15 },
-    { id: 16 },
-    { id: 17 },
-    { id: 18 },
-    { id: 19 },
-    { id: 20 },
-    { id: 21 },
-    { id: 22 },
-    { id: 23 },
-    { id: 24 },
-    { id: 25 },
-    { id: 26 },
-    { id: 27 },
-    { id: 28 },
-    { id: 29 },
-    { id: 30 },
-    { id: 31 },
-    { id: 32 },
-    { id: 33 },
-    { id: 34 },
-    { id: 35 },
-    { id: 36 },
-    { id: 37 },
-    { id: 38 },
-    { id: 39 },
-    { id: 40 },
-    { id: 41 },
-    { id: 42 },
-    { id: 43 },
-    { id: 44 },
-    { id: 45 },
-    { id: 46 },
-    { id: 47 },
-    { id: 48 },
-    { id: 49 },
-    { id: 50 }
-  ];
 });
--- www/js/services.js
+++ www/js/services.js
@@ -7,50 +7,128 @@ angular.module('starter.services', [])
   var publicacoes = [{
     id: 0,
     nome: 'Ben Sparrow',
-    data: "3 de maio de 2016",
+    data: Date.parse("May 3, 1996"),
+    likes: [],
     descricao: 'You on your way?',
     face: 'img/ben.png',
-    foto: 'img/photo/12917836_1011652265550934_765238525_n.jpg'
+    foto: 'img/photo/12917836_1011652265550934_765238525_n.jpg',
+    comments: [
+      {
+        nome: 'Zeca',
+        msg: 'Gostei do parque!',
+        data: Date.parse("January 1, 2006")
+      },
+      {
+        nome: 'Zeca',
+        msg: 'Vamos para lá?',
+        data: Date.parse("January 1, 2013")
+      },
+      {
+        nome: 'Zeca',
+        msg: 'Que mensagem velha...',
+        data: Date.parse("January 20, 2016")
+      },
+      {
+        nome: 'Eduardo',
+        msg: 'Que mensagem velha...',
+        data: moment().startOf('day')
+      },
+      {
+        nome: 'Fernando',
+        msg: 'Que mensagem velha mesmo...',
+        data: moment().startOf('hour')
+      },
+    ]
   }, {
     id: 1,
     nome: 'Max Lynx',
-    data: "1 de  maio de 2016",
+    data: Date.parse("May 1, 2016"),
+    likes: ['Anônimo'],
     descricao: 'Hey, it\'s me',
     face: 'img/max.png',
     foto: 'img/photo/12959915_1025189597548708_2087513361_n.jpg'
   }, {
     id: 2,
     nome: 'Adam Bradleyson',
-    data: "21 de abril de 2016",
+    data: Date.parse("April 21, 2016"),
+    likes: ['Anônimo', 'Chiu', 'João'],
     descricao: 'I should buy a boat',
     face: 'img/adam.jpg',
     foto: 'img/photo/12959949_697888996981196_1152474910_n.jpg'
   }, {
     id: 3,
     nome: 'Perry Governor',
-    data: "25 de março de 2016",
+    data: Date.parse("March 25, 2016"),
+    likes: ['Chiu', 'Pedro', 'José', 'Maria'],
     descricao: 'Look at my mukluks!',
     face: 'img/perry.png',
     foto: 'img/photo/12959953_471822229694124_1099346753_n.jpg'
   }, {
     id: 4,
     nome: 'Thiago Bezerra',
-    data: '16 de fevereiro de 2016',
+    data: Date.parse("February 1, 2016"),
+    likes: ['Paulo', 'Alice', 'Bob', 'Bruna'],
     descricao: 'I love coffee.',
     face: 'img/thiago.jpg',
     foto: 'img/photo/13109037_1606067166351284_793594177_n.jpg'
   }, {
     id: 5,
     nome: 'Mike Harrington',
-    data: "10 de fevereiro de 2016",
+    data: Date.parse("February 10, 2006"),
+    likes: [],
     descricao: 'This is wicked good ice cream.',
     face: 'img/mike.png',
     foto: 'img/photo/12976678_1114384848619243_270315244_n.jpg'
   }];
 
   return {
+    add: function(nome, descricao, face, foto) {
+      var pub = {
+        id: Math.ceil((Math.random() * 100000000000000)),
+        nome: nome,
+        data: new Date(),
+        likes: [],
+        descricao: descricao,
+        face: face,
+        foto: foto,
+      };
+
+      publicacoes.unshift(pub);
+    },
+
+    addComment: function(publicacaoId, nome, comentario) {
+      // arguments mostra todos os argumentos recebidos pela função
+      // console.log(arguments);
+      var publicacao = this.get(publicacaoId);
+      if (publicacao.comments == undefined) {
+        publicacao.comments = [];
+      }
+
+      publicacao.comments.push({
+        nome: nome,
+        msg: comentario,
+        data: new Date(),
+      });
+    },
     all: function() {
       return publicacoes;
+    },
+    verificarApoio: function(publicacaoId, nome) {
+      var publicacao = this.get(publicacaoId);
+      var index = publicacao.likes.indexOf(nome);
+
+      return (index != -1);
+    },
+    apoiarToggle: function(publicacaoId, nome) {
+      var publicacao = this.get(publicacaoId);
+      var index = publicacao.likes.indexOf(nome);
+
+      if (index == -1) {
+        publicacao.likes.push(nome);
+      } else {
+        // remove o elemento
+        publicacao.likes.splice(index, 1);
+      }
     },
     get: function(publicacaoId) {
       for (var i = 0; i < publicacoes.length; i++) {
